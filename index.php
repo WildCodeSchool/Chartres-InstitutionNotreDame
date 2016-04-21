9a639ed4bb2f128c94215eb40f4e4d880a621b0e
julianxiaoyu <julianmoreau@yahoo.fr>
2016-04-21 09:42:20 +0200

    index.html

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 get_header(); 
 ?>
@@ -6,8 +6,4 @@ get_header();
 <?php
 get_footer(); 
 ?>
-=======
-<?php get_header(); ?>
 
-<?php get_footer(); ?>
->>>>>>> 16f207fba24017a32c34d32818c5155fbe02182a
