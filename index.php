539c88aec1319deee853d4b1ad3aa7d1a8ec4644
Dessaint <franck.dessaint@gmail.com>
2016-04-21 14:08:07 +0200

    Ajout des média dans la page accueil

--- index.php
+++ index.php
@@ -212,12 +212,16 @@
 	</div>
     <div class="col-md-4">
         <h4>Vidéo</h4>
+        <iframe height="300" width="350" src="https://www.youtube.com/embed/videoseries?list=UUr2_AMouXAv838HHARFw93g" frameborder="0" allowfullscreen></iframe> 
     </div>
     <div class="col-md-4">
         <h4>Facebook</h4>
+       	<div class="fb-page" data-href="https://www.facebook.com/indchartres" data-tabs="timeline" data-width="350" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/indchartres"><a href="https://www.facebook.com/indchartres"></a></blockquote></div></div>
     </div>
     <div class="col-md-4">
         <h4>Twitter</h4>
+         <a class="twitter-timeline"  href="https://twitter.com/IndChartres" data-widget-id="716977401792700416">Tweets de @IndChartres</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     </div>
 </div>
 
