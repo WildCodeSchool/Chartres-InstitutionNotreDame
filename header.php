9b192b31dd27b115153c05104114b160b53d6801
Dessaint <franck.dessaint@gmail.com>
2016-04-21 11:45:15 +0200

    Petite modifications sur le footer

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 		<meta charset="UTF-8">
 		<title>Institution Notre Dame</title>
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.css">
+
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/script/jquery.js"></script>
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/script/bootstrap.js"></script>
@@ -16,7 +17,7 @@
 				<div class="row">
 					<div class="header-logo col-lg-2 col-md-2 col-sm-2 col-xs-3">
 						<a href="index.php">
-							<img class="img-responsive" src="img/logo-tr2.png" alt="logo du site institution notre dame">
+							<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/logo-tr2.png" alt="logo du site institution notre dame">
 						</a>
 					</div>
 					<div class="header-title col-lg-6 col-md-6 col-sm-10 col-xs-9">
@@ -36,8 +37,12 @@
 		</div>
 		<!-- END HEADER -->
 		<div class="col-md-12 menu-nav">
-			<div class="col-md-2 menu-nav-part" id="menu1">
-				<h4>Accueil</h4>
+			<div class="col-md-1 menu-nav-part" id="menu00">
+				<a href="index.php"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-accueil.png"</a>
+			</div>
+			<div class="col-md-1 menu-nav-part" id="menu0"></div>
+			<div class="col-md-1 menu-nav-part" id="menu1">
+				<h4>École</h4>
 			</div>
 			<div class="col-md-1 menu-nav-part" id="menu3">
 				<h4>Collège</h4>
@@ -45,8 +50,8 @@
 			<div class="col-md-1 menu-nav-part" id="menu2">
 				<h4>Lycées</h4>
 			</div>
-			<div class="col-md-2 menu-nav-part" id="menu4">
-				<h4>ESCC Supérieur</h4>
+			<div class="col-md-1 menu-nav-part" id="menu4">
+				<h4>ESCC</h4>
 			</div>
 			<div class="col-md-1 menu-nav-part">
 				<h4>Apel</h4>
@@ -57,6 +62,7 @@
 			<div class="col-md-1 menu-nav-part">
 				<h4>Contact</h4>
 			</div>
+			<div class="col-md-1 menu-nav-part" id="menu0"></div>
 			<div class="col-md-2">
 			<form class="search_form"><?php get_search_form(); ?></form>
 			</div>
