8ee20728880b8af4642d887a4b91a2bd3f0d2779
Dessaint <franck.dessaint@gmail.com>
2016-04-21 16:51:54 +0200

    Ajouts des premiers article pour démo

--- template1.php
+++ template1.php
@@ -92,6 +92,7 @@ Template Name: accueil
     ?> 
 
     <div class="col-md-12 main-top">
+        <h4><?php the_title(); ?></h4>
         <?php the_content(); ?>
     </div>
 
@@ -154,8 +155,11 @@ Template Name: accueil
         ?> 
 
         <div class="col-md-6 main-actu">
-            <?php the_content(); ?>
+            <img src="<?php the_post_thumbnail_url(); ?>">
+            <h4><?php the_title() ;?></h4>
+            <p><?php the_excerpt();?></p>
         </div>
+            
 
         <?php
             endwhile;
@@ -212,17 +216,21 @@ Template Name: accueil
 </div>
 
 <div class="col-md-12 global-large-div">
-	<div class="col-md-12 global-title">
-		<h3>MEDIA</h3>
-	</div>
+    <div class="col-md-12 global-title">
+        <h3>MEDIA</h3>
+    </div>
     <div class="col-md-4">
         <h4>Vidéo</h4>
+        <iframe height="300" width="350" src="https://www.youtube.com/embed/videoseries?list=UUr2_AMouXAv838HHARFw93g" frameborder="0" allowfullscreen></iframe> 
     </div>
     <div class="col-md-4">
         <h4>Facebook</h4>
+        <div class="fb-page" data-href="https://www.facebook.com/indchartres" data-tabs="timeline" data-width="350" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/indchartres"><a href="https://www.facebook.com/indchartres"></a></blockquote></div></div>
     </div>
     <div class="col-md-4">
         <h4>Twitter</h4>
+         <a class="twitter-timeline"  href="https://twitter.com/IndChartres" data-widget-id="716977401792700416">Tweets de @IndChartres</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     </div>
 </div>
 
