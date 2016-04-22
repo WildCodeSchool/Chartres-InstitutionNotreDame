83a6dea6abf850a0b87a1287515ec7ff7efa4293
PTony <jokr.01@gmail.com>
2016-04-22 11:12:53 +0200

    suppression image à la une

--- accueil_etablissements.php
+++ accueil_etablissements.php
@@ -3,11 +3,6 @@
 Template Name: Accueil établissement
 */
 ?>
-<?php
-/*
-Template Name: template1
-*/
-?>
 
 <?php get_header(); ?>
 
@@ -158,7 +153,7 @@ Template Name: template1
         ?> 
 
         <div class="col-md-3 main-actu">
-        	<img src="<?php the_post_thumbnail_url(); ?>">
+            <h4><?php the_title(); ?></h4>
             <?php the_content(); ?>
         </div>
 
